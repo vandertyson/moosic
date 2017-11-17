c69a10beaf913e53dca0e3ebe75d76a3f8e6bc6c
Son <ptson117@gmail.com>
2017-11-17 13:14:01 +0700

    zzzzz

--- src/app/pages/classify/view.component.ts
+++ src/app/pages/classify/view.component.ts
@@ -29,6 +29,7 @@ export class ClassifyComponent {
     private abstractText = "";
     private result = []
     private result2 = {}
+    private keywords = []
     private lookup_labels = [
         {
             name: "aimx",
@@ -59,6 +60,8 @@ export class ClassifyComponent {
         this.lookup_labels.forEach(e => {
             this.result2[e.name] = []
         })
+        this.keywords = []
+        jQuery("#theText").html("")
     }
     ngAfterViewInit() {
         jQuery(this.myView.nativeElement).addClass("full-screen")
@@ -136,11 +139,19 @@ export class ClassifyComponent {
         this.result = []
         this.initResult()
         var text = jQuery(this.input.nativeElement).val()
-        var param = {
-            sentences: this.cleanData(text)
+        var cleanText = this.cleanData(text)
+        var p1 = []
+
+        cleanText.split(".").forEach(element => {
+            if (element && element != '')
+                p1.push(element)
+        });
+        var param1 = {
+            sentences: p1,
+            full: cleanText
         }
         jQuery(this.myView.nativeElement).find('.submit').toggle()
-        this.api.post("http://localhost:8000/recommended_system/test", JSON.stringify(param))
+        this.api.post("http://localhost:8000/recommended_system/test", JSON.stringify(param1))
             .map(res => res)
             .subscribe(
             r => {
@@ -156,6 +167,27 @@ export class ClassifyComponent {
                 for (var index = 0; index < result.sentences.length; index++) {
                     this.result2[result.class[index]].push(result.sentences[index])
                 }
+                this.keywords = result.keywords
+
+                var str = cleanText;
+                var places = [];
+                var showText = str;
+                // if (this.keywords) {
+                var bolds = this.keywords.map(obj => { return obj[0] })
+                // console.log(bolds)
+                for (var obj of this.keywords) {
+                    var place = {
+                        "start": str.indexOf(obj[1].trim()),
+                        "end": str.indexOf(obj[1].trim()) + obj[1].trim().length,
+                        "bold": obj[0],
+                        "rank": this.getRank(obj[0], bolds)
+                    }
+                    places.push(place);
+                }
+                // places[0].start += 1
+                // places[0].end += 1
+                showText = this.highlight(places, str)
+                document.getElementById("theText").innerHTML = showText;
                 jQuery(this.myView.nativeElement).find('.submit').toggle()
             },
             e => {
@@ -171,23 +203,63 @@ export class ClassifyComponent {
         var s = text
             .trim()
             .replace(/(\r\n|\n|\r)/gm, " ")
-            .replace(/et al./g, "and others ")
-            .replace(/e.g./g, "for examples ")
-            .replace(/i.e./g, "in other words ")
-            .replace(/viz./g, "namely ")
-            .replace(/vs./g, "against ")
-            .replace(/etc./g, "and so forth ")
-            .replace(/N.B./g, "note well")
-            .replace(/cf./g, "compare with ")
-            .replace(/q.v./g, "which see ")
+            .replace(new RegExp("-", 'g'), " ")
+            // .replace(/et al./g, "and others ")
+            // .replace(/e.g./g, "for examples ")
+            // .replace(/i.e./g, "in other words ")
+            // .replace(/viz./g, "namely ")
+            // .replace(/vs./g, "against ")
+            // .replace(/etc./g, "and so forth ")
+            // .replace(/N.B./g, "note well")
+            // .replace(/cf./g, "compare with ")
+            // .replace(/q.v./g, "which see ")
             .trim()
-            .split(".")
+            .toLocaleLowerCase()
+        //     .split(".")
 
-        s.forEach(element => {
-            if (element && element != '')
-                result.push(element)
-        });
-        return result
+        // s.forEach(element => {
+        //     if (element && element != '')
+        //         result.push(element)
+        // });
+        return s
     }
+
+    highlight(places, str) {
+        var starts = [], ends = [], links = [], ranks = []
+
+        places.forEach(place => {
+            starts.push(place.start);
+            ends.push(place.end);
+            ranks.push(place.rank)
+        });
+        var max = ranks.sort((a, b) => { return b.rank - a.rank })[0]
+        var min = ranks.sort((a, b) => { return b.rank - a.rank })[ranks.length - 1]        
+        return str.split('').map((chr, pos) => {
+            if (starts.indexOf(pos) != -1) {
+                // var num = links.pop()
+                // var w = num * 100 + ''
+                // var s = Math.min(num / 0.2, 36) + 'px'
+                var i = starts.indexOf(pos)
+                console.log(i, this.keywords[i], places[i])
+
+                var num = places[i].rank
+                var w = 700 * (num - min)
+                var s = 36 - num*2;
+                if (s < 14) s = 14
+                // if (w < 100) w = 100
+                chr = '<a><b class="cursor" style="font-weight:' + w + ' ;font-size:' + s + 'px">' + chr;
+            }
+            if (ends.indexOf(pos) != -1) {
+                chr = '</b></a>' + chr;
+            }
+            return chr;
+        }).join('')
+    }
+
+    getRank(value, arr) {
+        var sorted = arr.sort((a, b) => { return b - a })
+        return sorted.indexOf(value) + 1
+    }
+
 }
 
